a2d77df2ae223b1b9addf1366881693f8c6b1fcf
truelav <madan.serghei@yahoo.com>
2018-09-19 12:16:11 -0700

    basic components ready need to make a get request to API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
+import { Provider } from './context';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider>
+        <Router>
+          <React.Fragment>
+
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ src/context.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+      tracks_list: [
+            {
+              track: { track_name: 'abc' },
+            }, 
+            { 
+              track: { track_name: '1234' },
+            } 
+      ],
+      heading: 'Top 10 tracks'
+  }
+
+  componentDidMount(){
+      axios.get(``)
+            .then( res => console.log(res.data) )
+            .catch( err => console.log(err) )
+  }
+
+  render() {
+    return (
+        <Context.Provider value={this.state}>
+            {this.props.children}
+        </Context.Provider>
+    )
+  }
+}
+
+export const Consumer = Context.Consumer;
